5bcd5ad565abec819e9a96e472d084bfd0f64eb0
Hasan Şyhemuhammed <hasanseyh65@gmail.com>
2023-10-15 23:03:49 +0300

    Update ass_assembly.sh

--- ass-assembly/ass_assembly.sh
+++ ass-assembly/ass_assembly.sh
@@ -1,46 +1,45 @@
 #!/bin/sh
 
 # MIT License
-# Copyright (c) 2022 Hasan ŞyheMuhamet
+# author 2022 hasan seyhmuhammed
 
-# define local variables
-format_output_obj=".o";
-# NOTE: if your format file is .s change this value into '.s'
-format_original_asm=".asm";
-program_name="alex_out";
-run_command="run"
+# important variables
+OUT_OBJECT_FORMAT=".o";
+# IF YOUR FILE EXTENSION IS ENDING WITH filename.s CHANGE THE VALUE OF FILE_EXTENSION VARIABLE INTO ".s"
+FILE_EXTENSION=".asm";
+PROGRAM_NAME="assembly";
+RUN_COMMAND="run"
 
-# colors vairables
+# color variables
 BLACK='\033[0;30m'; # black color
 RED='\033[0;31m'; # red color
 GREEN='\033[0;32m'; # green color
 OUT='\033[0;33m'; # output color
 # end
 
-# print this error message if the value of $1 is empty.
+# print error message if the value of $1 is empty.
 if [ -z $1 ];
 then
-	echo "${RED}ERROR: no input files${BLACK}";
+	echo "${RED}ERROR: no input file${BLACK}";
 else
-	# check if $2 is empty compile asm file without execution.
-	if [ -z $2 ];
+	if [ -z $2 ]; # if $2 is empty compile assembly code without executing the output file.
 	then
-		echo "${RED}NOTE:${GREEN} compile without execution";
-		output=$1$format_output_obj;
-		nasm -f elf $1$format_original_asm;
-		ld -m elf_i386 -s -o $program_name $output;
+		echo "${RED}NOTE:${GREEN} compiling completed without executing.";
+		OUTPUT=$1$OUT_OBJECT_FORMAT;
+		nasm -f elf $1$FILE_EXTENSION;
+		ld -m elf_i386 -s -o $PROGRAM_NAME $OUTPUT;
 		rm $output; # remove o. file
 
 	# check if $2 equal run word if yes compile & execute the file.
-	elif [ $2 = "$run_command" ];
+	elif [ $2 = "$RUN_COMMAND" ];
 		then
-			echo "${RED}NOTE:${GREEN} compile and execute";
-			output=$1$format_output_obj;
-			nasm -f elf $1$format_original_asm;
-			ld -m elf_i386 -s -o $program_name $output;
+			echo "${RED}NOTE:${GREEN} compile and execute output file.";
+			OUTPUT=$1$OUT_OBJECT_FORMAT;
+			nasm -f elf $1$FILE_EXTENSION;
+			ld -m elf_i386 -s -o $PROGRAM_NAME $OUTPUT;
 			rm $output; # remove .o file
-			./$program_name # run output file
+			./$PROGRAM_NAME # run output file
 	else
-		echo "${RED}ERROR: no input files";
+		echo "${RED}ERROR: no input file";
 	fi
 fi
